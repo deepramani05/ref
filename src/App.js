0ec0a25b7b34f3d86b001728e195292af9096e53
deepramani05 <deepramani4653@gmail.com>
2023-08-01 20:58:32 +0530

    ref

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+
+  let name = useRef()
+  let number = useRef()
+  let mail = useRef()
+  let cource = useRef()
+  let city = useRef()
+  let date = useRef()
+
+  const manage = (e) => {
+    e.preventDefault();
+    console.log(name.current.value);
+    console.log(number.current.value);
+    console.log(mail.current.value);
+    console.log(cource.current.value);
+    console.log(city.current.value);
+    console.log(date.current.value);
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <form onSubmit={manage}>
+        <input type="text" ref={name} placeholder="Student Name" />  
+        <input type="number" ref={number} placeholder="Mobile Number" />
+        <input type="email" ref={mail} placeholder="E-mail ID" />
+        <input type="text" ref={cource} placeholder="Cource Name" />
+        <input type="text" ref={city} placeholder="City Name" />
+        <input type="date" ref={date} />
+        <input type="submit" value="Click" />
+      </form>
+
+      
+      
+    </>
+  )
+
 }
 
 export default App;
